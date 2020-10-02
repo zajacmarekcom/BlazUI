9ab99075e33e456d0d24db95e4d530179ca6d06f
Marek Zajac <contact@zajacmarek.com>
2020-10-02 21:20:50 +0200

    Added accordion, added list, changed size of buttons in navbar, updated icons, added new color variables

---
+++ src/BlazUI/BlazUI/Services/AccordionService.cs
@@ -0,0 +1,45 @@
+﻿using BlazUI.Components;
+using System.Collections.Generic;
+
+namespace BlazUI.Services
+{
+    public class AccordionService
+    {
+        private IList<AccordionItem> Items { get; }
+
+        public delegate void ItemOpened(AccordionItem item);
+        public event ItemOpened ItemOpenedHandler;
+
+        public AccordionService()
+        {
+            Items = new List<AccordionItem>();
+        }
+
+        public void AddChild(AccordionItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void OpenAll()
+        {
+            foreach(var item in Items)
+            {
+                item.Open();
+            }
+        }
+
+        public void CloseAll(AccordionItem exceptItem = null)
+        {
+            foreach(var item in Items)
+            {
+                if (item == exceptItem) continue;
+                item.Close();
+            }
+        }
+
+        public void Opened(AccordionItem item)
+        {
+            ItemOpenedHandler?.Invoke(item);
+        }
+    }
+}
