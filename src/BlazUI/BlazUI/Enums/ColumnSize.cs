950143f38cfe11ed4a5f60d66c1c3063b17bdfba
Marek Zajac <contact@zajacmarek.com>
2020-10-02 20:01:09 +0200

    Rows and columns component

---
+++ src/BlazUI/BlazUI/Enums/ColumnSize.cs
@@ -0,0 +1,34 @@
+﻿namespace BlazUI.Enums
+{
+    public enum ColumnSize
+    {
+        Col1 = 1,
+        Col2,
+        Col3,
+        Col4,
+        Col5,
+        Col6,
+        Col7,
+        Col8,
+        Col9,
+        Col10,
+        Col11,
+        Col12
+    }
+
+    public enum Offset
+    {
+        Offset0 = 0,
+        Offset1,
+        Offset2,
+        Offset3,
+        Offset4,
+        Offset5,
+        Offset6,
+        Offset7,
+        Offset8,
+        Offset9,
+        Offset10,
+        Offset11
+    }
+}
