6cdf5f844b4bdbf159c5b35d173bf27e664f2dfa
Marek Zajac <contact@zajacmarek.com>
2020-10-03 15:24:01 +0200

    Add logo to webite, fix AlwaysOpen for accordion, fix navbar styles, add style for code

--- src/BlazUI/BlazUI/Services/AccordionService.cs
+++ src/BlazUI/BlazUI/Services/AccordionService.cs
@@ -6,18 +6,24 @@ namespace BlazUI.Services
     public class AccordionService
     {
         private IList<AccordionItem> Items { get; }
+        private bool AlwaysOpen { get; }
 
         public delegate void ItemOpened(AccordionItem item);
         public event ItemOpened ItemOpenedHandler;
 
-        public AccordionService()
+        public AccordionService(bool alwaysOpen = false)
         {
             Items = new List<AccordionItem>();
+            AlwaysOpen = alwaysOpen;
         }
 
         public void AddChild(AccordionItem item)
         {
             Items.Add(item);
+            if (AlwaysOpen)
+            {
+                item.Open();
+            }
         }
 
         public void OpenAll()
